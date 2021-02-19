1d3ab211ace32e9acbdd8c00df1803928694f542
student <ronald.carson@ngc.com>
2021-02-19 00:06:10 +0000

    Add logic and test

---
+++ src/test/java/com/simplilearn/reviewanalyser/ReviewanalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.reviewanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewanalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
